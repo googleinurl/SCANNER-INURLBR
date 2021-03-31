fc3b63ba76a1bde044edd18df13461257dff5782
MrCl0wnLab <0x27null@gmail.com>
2021-03-30 23:47:58 -0300

    EndColor: $_SESSION["c0"]

--- src/functions/get_out.func.php
+++ src/functions/get_out.func.php
@@ -6,6 +6,7 @@ function __getOut($msg) {
     (!function_exists('__ircQuit') ? NULL :
     __ircQuit($_SESSION['config']['irc']));
     print_r($msg);
+    print($_SESSION["c0"]);
     exit(1);
  
 }
